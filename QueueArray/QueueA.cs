254e49be19c07379a54ddecff991a08e2431e065
Kewin Titus <kewintitus@gmail.com>
2022-09-06 07:14:46 +0530

    [FEAT] - Queqe Implementation started

---
+++ QueueArray/QueueA.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace QueueArray
+{
+    class QueueA
+    {
+        private int[] queueArray;
+        private int front;
+        private int rear;
+
+        public QueueA()
+        {
+            queueArray = new int[10];
+            front = -1;
+            rear = -1;
+        }
+        public QueueA(int maxSize)
+        {
+            queueArray = new int[maxSize];
+            front = -1;
+            rear= -1;
+        }
+        public bool IsEmpty()
+        {
+            return (front == -1 || front == rear + 1);
+        }
+
+        public bool IsFull()
+        {
+            return (rear == queueArray.Length -1);    
+        }
+
+        public int Size()
+        {
+            if (IsEmpty())
+            {
+                return 0;
+            }
+            else
+            {
+                return rear - front +1;
+            }
+        }
+
+    }
+
+}
