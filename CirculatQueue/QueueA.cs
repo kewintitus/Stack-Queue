4bb6e282e2693132bb3de75aab0c7ac4cbb13126
Kewin Titus <kewintitus@gmail.com>
2022-09-08 10:27:55 +0530

    circular queue implementation started

---
+++ CirculatQueue/QueueA.cs
@@ -0,0 +1,53 @@
+﻿namespace CircularQueue
+{
+    class QueueA
+    {
+        private int[] queueArray;
+        private int front;
+        private int rear;
+
+
+        public QueueA()
+        {
+            queueArray = new int[10]; ;
+            front = -1;
+            rear = -1;
+        }
+        public QueueA(int maxSize)
+        {
+            queueArray = new int[maxSize]; 
+            front = -1;
+            rear= -1;
+        }
+
+        public bool IsEmpty()
+        {
+            return front == -1;
+        }
+        public bool IsFull()
+        {
+            return ((front == 0 && rear == queueArray.Length - 1) || (front == rear + 1));
+        }
+        public void Insert(int x)
+        {
+            
+            if (IsFull())
+            {
+                Console.WriteLine("Queue overflow");
+                return;
+            }
+            if (front == -1)
+                front = 0;
+            if (rear == queueArray.Length - 1)
+            {
+                rear = 0;
+            }
+            else
+            {
+                rear = rear + 1;
+            }
+            queueArray[rear] = x;
+        }
+    }
+
+}
